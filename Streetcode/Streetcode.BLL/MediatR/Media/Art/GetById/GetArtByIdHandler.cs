101c768292a0a2951314ddfd862bddc0ce07f945
Valentyn Zamykhivskyi <76389693+petrobubka@users.noreply.github.com>
2023-10-14 19:06:47 +0300

    Update GetArtByIdHandler.cs

--- Streetcode/Streetcode.BLL/MediatR/Media/Art/GetById/GetArtByIdHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Media/Art/GetById/GetArtByIdHandler.cs
@@ -11,6 +11,7 @@ using Streetcode.DAL.Repositories.Interfaces.Base;
 
 namespace Streetcode.BLL.MediatR.Media.Art.GetById;
 
+
 public class GetArtByIdHandler : IRequestHandler<GetArtByIdQuery, Result<ArtDTO>>
 {
     private readonly IMapper _mapper;
